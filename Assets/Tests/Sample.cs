79af1c86aa38614bb7d4fcadfbf2344d21850208
Will Fawcett <wfawcett@gmail.com>
2020-01-29 21:57:14 -0700

    project commit

---
+++ Assets/Tests/Sample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Sample
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SampleSimplePasses()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        
+    }
+}
